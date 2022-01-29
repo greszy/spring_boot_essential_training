aca0b8e9da2bee752a7fad9133033b43b7d5a040
Gregory Szymanski <gregoryszy@gmail.com>
2022-01-29 13:13:13 -0500

    Adds a module with a CommandLineRunner FizzBuzz exercise example.

---
+++ fizz-buzz/src/test/java/com/gregoryszymanski/fizzbuzz/FizzBuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gregoryszymanski.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzBuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
