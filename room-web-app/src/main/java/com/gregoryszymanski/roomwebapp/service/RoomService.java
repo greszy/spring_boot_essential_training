63b993164f29649f0df4e8f00c0f9d96500d73d0
Gregory Szymanski <gregoryszy@gmail.com>
2022-01-29 21:05:34 -0500

    Adds required files and configuration to build an embedded database and refactors the web application to request room data from the database.

--- room-web-app/src/main/java/com/gregoryszymanski/roomwebapp/service/RoomService.java
+++ room-web-app/src/main/java/com/gregoryszymanski/roomwebapp/service/RoomService.java
@@ -1,5 +1,6 @@
 package com.gregoryszymanski.roomwebapp.service;
 
+import com.gregoryszymanski.roomwebapp.data.RoomRepository;
 import com.gregoryszymanski.roomwebapp.models.Room;
 import org.springframework.stereotype.Service;
 
@@ -8,16 +9,13 @@ import java.util.List;
 
 @Service
 public class RoomService {
-    //Here a list of rooms are generated. This list is later displayed in the rooms.html template.
-    private static final List<Room> rooms = new ArrayList<>();
+    private final RoomRepository roomRepository;
 
-    static {
-        for(int i=0;i<10;i++) {
-            rooms.add(new Room(i, "Room " +i,"R"+i, "Q"));
-        }
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
     }
 
     public List<Room> getAllRooms() {
-        return rooms;
+        return roomRepository.findAll();
     }
 }
